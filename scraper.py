3263752e6607df077d38fbbbea154454b3268f3c
Karina Antonio <karinafantonio@gmail.com>
2018-05-31 11:39:04 -0700

    Collect class rows

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from lxml import etree
+import lxml.html
 import requests
 
 scheduleResponse = requests.post(
@@ -9,3 +9,17 @@ scheduleResponse = requests.post(
         'openclasses': 'N'
     }
 )
+
+root = lxml.html.fromstring(scheduleResponse.content)
+
+allRows = root.cssselect('table.datadisplaytable > tr')
+
+COLUMNS = ('CRN', 'departmentID', 'title', 'units', 'activity', 'days', 'time', 'location', 'termLength', 'instructor', 'maxSeats', 'takenSeats', 'freeSeats')
+
+def isHeader(row):
+    return row.getchildren()[0].tag == 'th'
+
+def isExamRow(row):
+    return row.getchildren()[COLUMNS.index('activity')].text_content == 'EXAM'
+
+classRows = filter(lambda r: not isHeader(r) and not isExamRow(r), allRows)
